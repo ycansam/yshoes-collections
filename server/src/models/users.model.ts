5c210bca33c58eae6b92135c06501b7386caa3da
Yeray Candel <ycansam@epsg.upv.es>
2023-02-18 19:51:13 +0100

    arreglado modelo users runValitors a true

--- server/src/models/users.model.ts
+++ server/src/models/users.model.ts
@@ -3,10 +3,30 @@ import mongoose from 'mongoose';
 import IUser from '../interfaces/users.interface';
 const { Schema } = mongoose;
 
+const validateNoSpaces = (str: String): boolean => {
+    return str.indexOf(' ') === -1;
+}
+
+
 const UserModel = new Schema<IUser>({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateNoSpaces, "Name cannot contain spaces"],
+        minlength: 1,
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: [validateNoSpaces, "Password cannot contain spaces"],
+        minlength: 4,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     name: { type: String },
     surnames: { type: String },
     jwt_access: { type: String },
@@ -19,5 +39,5 @@ const UserModel = new Schema<IUser>({
 },
     { strict: true }
 );
-
+mongoose.set('runValidators', true)
 module.exports = mongoose.model('User', UserModel);
