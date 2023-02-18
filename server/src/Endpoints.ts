5310f44a984c426c9f6c3c6f295c2e625f75fa32
Yeray Candel <ycansam@epsg.upv.es>
2023-02-18 16:10:41 +0100

    creados endpoints products collections cart

--- server/src/Endpoints.ts
+++ server/src/Endpoints.ts
@@ -1,6 +1,7 @@
 export default class Endpoints {
     static readonly version = "/v1";
     static readonly DEFAULT_PATH = this.version + "/yshoes";
+
     static readonly USERS = {
         DEFAULT_PATH: this.DEFAULT_PATH + "/users",
         GET: "/",
@@ -9,4 +10,33 @@ export default class Endpoints {
         UPDATE: "/:id",
         DELETE: "/:id",
     };
+
+    static readonly CART = {
+        DEFAULT_PATH: this.DEFAULT_PATH + "/cart",
+        GET_USER_CART: "/:id_user",
+        UPDATE_USER_CART: "/:id_user",
+        CLEAR_USER_CART: "/:id_user",
+        ADD_PRODUCT: "/:id_user",
+        REMOVE_PRODUCT: ":id_user"
+    };
+
+    static readonly PRODUCTS = {
+        DEFAULT_PATH: this.DEFAULT_PATH + "/products",
+        GET: "/",
+        GET_ONE: "/:id",
+        CREATE: "/",
+        UPDATE: "/:id",
+        DELETE: "/:id",
+    };
+
+    static readonly COLLECTIONS = {
+        DEFAULT_PATH: this.DEFAULT_PATH + "/collections",
+        GET: "/",
+        GET_ONE: "/:id",
+        CREATE: "/",
+        UPDATE: "/:id",
+        DELETE: "/:id",
+    };
+
+
 }
